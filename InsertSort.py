a9cca1ac8a8d21142132dd1f9998b6186ca46948
Lyon-NEU <wanlplab@gmail.com>
2014-06-11 22:36:33 +0800

    modify

--- InsertSort.py
+++ InsertSort.py
@@ -14,7 +14,14 @@ def insertSort(arr):
 			arr[i+1]=arr[i]
 			i--
 		arr[i+1]=arr[i]
-
+def InsertionSort(A):
+	for j in range(1,len(A)):
+		key=A[j]
+		i=j-1
+		while (i>=0) and (A[i]>key):
+			A[i+1]=A[i]
+			i=i-1
+		A[i+1]=key	
 def main():
 	"""
 	test
