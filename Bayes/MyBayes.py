cbb817d76f7ed950307a603b2e0815ce4420b1b7
Lyon-NEU <wanlplab@gmail.com>
2014-06-21 22:48:41 +0800

    aaa

--- Bayes/MyBayes.py
+++ Bayes/MyBayes.py
@@ -51,3 +51,16 @@ def trainNB0(trainMatrix,trainCategory):
     p1Vect =log(p1Num/p1Denom)
     p0Vect=log(p0Num/p0Denom)
     return p0Vect,p1Vect,pAbusive
+def classifyNB(vec2Classify,p0Vec,p1Vec,pClass1):
+    p1=sum(vec2Classify*p1Vec)+log(pClass1)
+    p0=sum(vec2Classify*p0Vec)+long(1.0-pClass1)
+    if p1>p0:
+        return 1
+    else:
+        return 0
+def bagOfWords2VecMN(vocabList,inputSet):
+    returnVec=[0]*len(vocabList)
+    for word in inputSet:
+        if word in vocabList:
+            returnVec[vocabList.index(word)]+=1
+    return returnVec
